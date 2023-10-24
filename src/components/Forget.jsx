b7b4bc9eb6e63a4615eb099fd656951ae38771b7
vimal <selvamvimaldz1@gmail.com>
2023-10-24 14:00:30 +0530

    add toast for rest of code

--- src/components/Forget.jsx
+++ src/components/Forget.jsx
@@ -4,12 +4,26 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import { UserDetailContext } from './userDetailContext';
 import ApiService from '../common/ApiService';
+import { toast } from 'react-toastify';
 function Forget() {
 
   const { mail, setMail } = useContext(UserDetailContext)
   // const navigate = useNavigate()
   const mailSend = async () => {
-    await ApiService.post('/forgetpass', { email: mail })
+   
+   try {
+    const res= await ApiService.post('/forgetpass', { email: mail })
+    if(res.status===200){
+     toast.success('check your mail')
+    } 
+   } catch (error) {
+    if(error.response.data.status===400){
+      toast.error('Invalid mail')
+    }else{
+      toast.error(error.response.data.message);
+    }
+   }
+   
     //  await axios.post('http://localhost:8000/forgetpass', { email: mail })
     // if (forgetFalg.data.otp) {
     //   navigate('/resetpassword')
@@ -19,7 +33,7 @@ function Forget() {
     <>
       <form className='reset-form'>
         <div className='reset-input form-floating'>
-        <input type='text' className='form-control' value={mail}  onChange={(e) => setMail(e.target.value)} />
+        <input type='text'  className='form-control' value={mail}  onChange={(e) => setMail(e.target.value)} />
         <label className='floatingInput'>Enter your mail</label>
         <Button onClick={() =>  mailSend() }>send</Button>
         </div>
--- src/components/ResePassword.jsx
+++ src/components/ResePassword.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap';
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { UserDetailContext } from './userDetailContext';
 import ApiService from '../common/ApiService';
+import { toast } from 'react-toastify';
 
 function ResePassword() {
+  const navigate=useNavigate();
   const [OTP, setOTP] = useState();
   // const[mail,setMail]=useState("");
   // const [pass, setPass] = useState("");
@@ -33,20 +35,31 @@ function ResePassword() {
   }, [])
   const changePassword = async (e) => {
     e.preventDefault()
-    await ApiService.post('/forgetpass/updatepassword', {
-      email: mail,
-      password: pass
-    })
-    // console.log(res.data);
-    sessionStorage.getItem('forgetToken').clear()
-    //  forgetToken.clear
+   
+    try {
+      const res= await ApiService.post('/forgetpass/updatepassword', {
+        email: mail,
+        password: pass
+      })
+      if(res.status===200){
+        toast.success('Password changed');
+        setPass(" ")
+        navigate('/')
+      }   
+    } catch (error) {
+      if(error.response.data.status===400){
+        toast.error('Invalid user')
+      }else{
+        toast.error(error.response.data.message);
+      }
+    }
   }
   return (<>
     {
       OTP ?
         <form className='reset-form ' >
           <div className='reset-input form-floating'>
-            <input type='text' className='form-control'
+            <input type='text' className='form-control' value={pass}
               onChange={(e) => setPass(e.target.value)} />
             <label className='floatingInput'>Enter new password</label>
             <Button onClick={(e) => changePassword(e)}>change</Button>
